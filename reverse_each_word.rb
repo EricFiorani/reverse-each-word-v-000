35eddae3cfd4e80492b009b0947f37140231d6d0
Eric Fiorani <efiorani305@gmail.com>
2018-04-23 16:27:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,3 +2,8 @@ def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   sentence1.split.collect {|word| word.reverse}.join(" ")
 end
+
+def reverse_each_word(sentence1)
+  sentence1 = "iH ,niaga tsuj gnikam erus s'ti !desrever"
+  sentence1.split.collect {|word| word.reverse}.join(" ")
+end
